a594e6a1ef71c3d187f133305a46543f615a1c16
Kirill M <kmuravskiy@gmail.com>
2018-08-08 22:45:21 -0400

    UI started. Added NPC shredder. Added a sprite for  gopnik nest - will be used for placement planning.

---
+++ Assets/Environments/Scripts/NPCShredder.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCShredder : MonoBehaviour {
+
+    private void OnTriggerEnter2D(Collider2D enteredCollider)
+    {
+        if (enteredCollider.gameObject.layer == 8)
+        {
+            Destroy(enteredCollider.gameObject);
+        }
+    }
+}
