d36eb5af114d0387996f79322b3407e415076909
Kirill M <kmuravskiy@gmail.com>
2018-09-05 21:14:12 -0400

    Added gopnik nest placement button. The nest now follows the mouse pre-placement!

---
+++ Assets/Environments/Scripts/BuildingPlacer.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingPlacer : MonoBehaviour {
+
+    [SerializeField] GameObject selectedBuilding;
+    [SerializeField] Transform yard;
+
+    SpriteRenderer buildingSpriteRenderer;
+    Camera mainCam;
+    bool isPlacingBuilding;
+
+    private void Start()
+    {
+        mainCam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+        if (isPlacingBuilding)
+        {
+            // Use the selected building and "carry" it around with the mouse pos
+            if (selectedBuilding != null)
+            {
+                Vector3 mouseWorldPos = mainCam.ScreenToWorldPoint(Input.mousePosition);
+                Vector2 pos = mouseWorldPos;
+                selectedBuilding.transform.position = pos;
+                ColorBuildingRed();
+            }
+        }
+	}
+
+    public void PlaceBuilding(GameObject buildingToPlace)
+    {
+        Debug.Log("Placing a " + buildingToPlace.name + " in the scene");
+        isPlacingBuilding = true;
+        Vector3 mouseWorldPos = mainCam.ScreenToWorldPoint(Input.mousePosition);
+        selectedBuilding = Instantiate(buildingToPlace, mouseWorldPos, Quaternion.identity, yard);
+        buildingSpriteRenderer = selectedBuilding.GetComponent<SpriteRenderer>();
+        selectedBuilding.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
+    }
+
+    void ColorBuildingRed()
+    {
+        Color negativeRed = new Color(1, 0, 0, 0.5f);
+        if (buildingSpriteRenderer.color != negativeRed)
+        {
+            buildingSpriteRenderer.color = negativeRed;
+        }
+        
+    }
+
+    void ColorBuildingGreen()
+    {
+        Color positiveGreen = new Color(0, 1, 0, 0.5f);
+        if (buildingSpriteRenderer.color != positiveGreen)
+        {
+            buildingSpriteRenderer.color = positiveGreen;
+        }
+
+    }
+}
---
+++ Assets/Environments/Scripts/Map.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Map : MonoBehaviour {
+
+    public static Map Instance;
+
+    [Header("Building placement parents")]
+    public Transform gopnikNestParent;
+
+	// Use this for initialization
+	void Start () {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else if (Instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
