b379c829a558bd183c744130fab3a9c2c473db5f
Kirill M <kmuravskiy@gmail.com>
2018-10-17 21:13:12 -0400

    Added a wooden box that will be used for prototyping carrying/dropping items by characters.

---
+++ Assets/Main Character/Scripts/MCharCarry.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MCharCarry : MonoBehaviour
+{
+    [SerializeField] GameObject currentItem;
+
+    public void PickUpItem()
+    {
+
+    }
+
+    public void DropItem()
+    {
+
+    }
+
+    public void ApplyItem()
+    {
+
+    }
+}
