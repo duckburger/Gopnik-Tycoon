a0ade02ca1ef15feefe6e5b5d40d4f329b6bb8af
Kirill M <kmuravskiy@gmail.com>
2019-02-06 22:39:16 -0500

    Health bar now jiggles when its value is updated.

--- Assets/UI/HealthBar/HealthBar.cs
+++ Assets/UI/HealthBar/HealthBar.cs
@@ -10,12 +10,16 @@ public class HealthBar : MonoBehaviour
     [Space]
     [SerializeField] float maxVal;
     [SerializeField] float currentVal;
-    
+    [Space]
+    [SerializeField] bool shake;
+
+    RectTransform myRect;
     CanvasGroup canvasGroup;
 
     private void Awake()
     {
         this.canvasGroup = this.GetComponent<CanvasGroup>();
+        this.myRect = this.transform as RectTransform;
     }
 
     public void Init(float maxVal)
@@ -31,12 +35,16 @@ public class HealthBar : MonoBehaviour
         this.currentVal = newAmount;
         float normalizedVav = newAmount / this.maxVal;
         this.canvasGroup.alpha = 1;
+        if (this.shake)
+        {
+            LeanTween.moveX(this.myRect, this.myRect.localPosition.x + 0.002f, 0.1f).setEase(LeanTweenType.easeInOutBounce).setLoopPingPong(1);
+        }
         LeanTween.value(this.fgFill.fillAmount, normalizedVav, 0.12f).setOnUpdate((float val) => this.fgFill.fillAmount = val).setEase(LeanTweenType.easeInOutSine);
         LeanTween.value(this.bgFill.fillAmount, normalizedVav, 0.1f).setOnUpdate((float val) => this.bgFill.fillAmount = val)
             .setEase(LeanTweenType.easeInOutSine).setDelay(0.2f)
             .setOnComplete(() => 
             {
-                LeanTween.alphaCanvas(this.canvasGroup, 0, 0.1f).setDelay(3f);
+                LeanTween.alphaCanvas(this.canvasGroup, 0, 0.1f).setDelay(5f);
             });
     }
 
